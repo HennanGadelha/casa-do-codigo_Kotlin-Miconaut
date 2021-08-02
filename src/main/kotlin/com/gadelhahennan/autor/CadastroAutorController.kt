1f14812d11a699d5c8131a0f5c3b277f196bf73c
Hennan Gadelha <hennanhennan@gmail.com>
2021-08-02 13:51:52 -0300

    retornando 201 apos cadastro de autor

--- src/main/kotlin/com/gadelhahennan/autor/CadastroAutorController.kt
+++ src/main/kotlin/com/gadelhahennan/autor/CadastroAutorController.kt
@@ -1,9 +1,11 @@
 package com.gadelhahennan.autor
 
 import com.gadelhahennan.autor.dtos.AutorRequest
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import java.util.*
 import javax.validation.Valid
 
 @Controller("/api/v1/autores")
@@ -11,11 +13,15 @@ open class CadastroAutorController(val autorRepository: AutorRepository) {
 
 
     @Post("/cadastro")
-   open  fun cadastra(@Body @Valid request: AutorRequest){
+   open  fun cadastra(@Body @Valid request: AutorRequest) : HttpResponse<Any>{
 
         val autor = request.toModel()
         autorRepository.save(autor)
 
+        return HttpResponse.created(location(autor.autorId))
+
     }
+
+    private fun location(autorUUID: UUID) = HttpResponse.uri("/api/v1/autores/$autorUUID")
 
 }
